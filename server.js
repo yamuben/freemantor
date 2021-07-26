1b30caa474cda1c6e6a41ac126843c384eca6763
yamuben <yamubbenjamin@gmail.comn>
2021-07-26 22:50:35 +0200

    fixing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./server/routes/userRoute";
+import userRouter from "./server/routes/userRoute.js";
 import bodyParser from "body-parser";
 
 
