8a9bf0dbc2459987bf39d2782c789848ef38395a
yamuben <yamubbenjamin@gmail.comn>
2021-07-26 22:43:35 +0200

    fixing

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.use(bodyParser.json());
 app.use("/freementor/v1/user",userRouter);
 
 
+app.use('/',(req,res)=>{
+    res.status(404).send({
+        statu:404,
+        message:"this route does not exist"
+    })
+})
+
+
 const port=process.env.PORT;
 const databaseUrl=process.env.DATABASE;
 
